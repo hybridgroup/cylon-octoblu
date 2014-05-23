7241b6dfc2e79c7c1a95d1fc73e78167e6e74a20
Andrew Stewart <andrew@stwrt.ca>
2014-05-23 11:47:22 -0700

    Improve test harness

--- test/support/globals.js
+++ test/support/globals.js
@@ -17,11 +17,12 @@ global.expect = chai.expect;
 global.assert = chai.assert;
 global.AssertionError = chai.AssertionError;
 global.sinon = require('sinon');
+global.spy = sinon.spy;
+global.stub = sinon.stub;
 
 // can be used by test modules to require production modules,
 // relative to the base path (where the Gruntfile.js also lives)
 global.source = function (src) {
-  console.log('source loading: ' +  src)
   var resource = path.normalize('../../lib/' + src);
 
   return require(resource);
