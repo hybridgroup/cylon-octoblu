886c36df7b484c6582945193daa4357a63ebc477
edgarsilva <edgar.osc@gmail.com>
2014-10-16 11:28:15 -0500

    Added fluent example.

--- examples/skynet-blink-fluent-style.js
+++ examples/skynet-blink-fluent-style.js
@@ -1,23 +1,25 @@
-var Cylon = require('cylon');
+var cylon = require('cylon');
 
-Cylon.robot({
+cylon.robot({
   connections: [
     { name: 'arduino', adaptor: 'firmata', port: '/dev/ttyACM0' },
     { name: 'skynet', adaptor: 'skynet', uuid: "742401f1-87a4-11e3-834d-670dadc0ddbf", token: "xjq9h3yzhemf5hfrme8y08fh0sm50zfr" }
   ],
 
   device: { name: 'led', driver: 'led', pin: 13, connection: 'arduino' },
+})
 
-  work: function(my) {
+.on('ready', function(robot) {
     console.log("connected...");
-    my.skynet.on('message', function(data) {
+    robot.skynet.on('message', function(data) {
       console.log(data);
       if(data.message.red === 'on') {
-        my.led.turnOn();
+        robot.led.turnOn();
       }
-      else if(data.message.red ==='off') {
-        my.led.turnOff();
+      else if(data.message.red === 'off') {
+        robot.led.turnOff();
       }
     });
-  }
-}).start();
+})
+
+.start();
